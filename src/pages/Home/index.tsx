f445508c78652b023e95e3bbbb3c9e9408e3f8dc
Talles Diniz <tallessdiniz@gmail.com>
2026-04-22 00:35:59 -0300

    feat: exibe as imagens do banco na página home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,67 @@
 import { Container } from "../../components/Container";
+import { useState, useEffect } from 'react';
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { Link } from "react-router-dom";
+
+interface CarsProps {
+    id: string;
+    name: string;
+    year: string;
+    uid: string;
+    price: number | string;
+    city: string;
+    km: string;
+    images: string[];
+}
+
+
 
 export function Home() {
-    return(
+    const [cars, setCars] = useState<CarsProps[]>([]);
+    const [loadImages, setLoadImages] = useState<string[]>([])
+
+    useEffect(() => {
+
+        function loadCars() {
+            const carsRef = collection(db, "cars")
+            const queryRef = query(carsRef, orderBy("created", "desc"))
+
+            getDocs(queryRef)
+                .then((snapshot) => {
+                    let listCars = [] as CarsProps[];
+
+                    snapshot.forEach(doc => {
+                        listCars.push({
+                            id: doc.id,
+                            name: doc.data().name,
+                            year: doc.data().year,
+                            km: doc.data().km,
+                            city: doc.data().city,
+                            price: doc.data().price,
+                            images: doc.data().images,
+                            uid: doc.data().uid
+                        })
+                    })
+
+                    setCars(listCars)
+                })
+        }
+
+        loadCars()
+    }, [])
+
+    function handleImageLoad(id: string) {
+        setLoadImages((prevImageLoaded) => [...prevImageLoaded, id])
+    }
+
+    return (
         <Container>
             <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
-                <input 
-                className="w-full border rounded-lg h-9 px-3 outline-none "
-                type="text" 
-                placeholder="Digite o nome do carro..."
+                <input
+                    className="w-full border rounded-lg h-9 px-3 outline-none "
+                    type="text"
+                    placeholder="Digite o nome do carro..."
                 />
                 <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg cursor-pointer">
                     Buscar
@@ -19,32 +73,42 @@ export function Home() {
             </h1>
 
             <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                <section className="w-full bg-white rounded-lg">
-                    <img
-                      className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                      src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2026/202604/20260402/hondacivic1516vturbogasolinatouring4pcvt-wmimagem13294582973.jpg?s=fill&w=552&h=414&q=60"
-                      alt="Carro"
 
-                    />
-                    <p className="font-bold mt-1 mb-2 px-2 ">Honda Civic</p>
-                    
-                    <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 2019/2020 | 66.000 km </span>
-                        <strong className="text-black font-medium text-xl">R$ 122.900</strong>
-                    </div>
+                {cars.map(car => (
+                    <Link key={car.id} to={`/car/${car.id}`}>
+                        <section  className="w-full bg-white rounded-lg">
+                            <div 
+                            className="w-full h-72 rounded-lg bg-slate-200">
+                            </div>
+                            <img
+                                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                                src={car.images[0]}
+                                alt="Carro"
+                                onLoad={ () => handleImageLoad(car.id)}
 
-                    <div className="w-full h-px bg-slate-200 my-2"></div>
+                            />
+                            <p className="font-bold mt-1 mb-2 px-2 ">{car.name}</p>
 
-                    <div className="px-2 pb-2">
-                        <span className="text-black">
-                            Mogi das Cruzes - SP
-                        </span>
-                    </div>
-                </section>
-            </main> 
+                            <div className="flex flex-col px-2">
+                                <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km </span>
+                                <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+                            </div>
 
-        </Container> 
-            
-       
+                            <div className="w-full h-px bg-slate-200 my-2"></div>
+
+                            <div className="px-2 pb-2">
+                                <span className="text-black">
+                                    {car.city}
+                                </span>
+                            </div>
+                        </section>
+                    </Link>
+                ))}
+
+            </main>
+
+        </Container>
+
+
     )
 }
