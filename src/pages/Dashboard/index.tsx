afd2ae75ea6d054b9e8e2112c22572cf83222e61
Talles Diniz <tallessdiniz@gmail.com>
2026-04-25 18:26:08 -0300

    feat: criação da tela Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,111 @@
 import { Container } from "../../components/Container"
 import { DashboardHeader } from "../../components/PainelHeader"
+import { FiTrash2 } from "react-icons/fi"
+import { useEffect, useState, useContext } from 'react'
+import { collection, query, getDocs, where, doc, deleteDoc  } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { AuthContext } from "../../context/Authcontext";
+
+interface CarProps {
+    id: string;
+    year: string;
+    name: string;
+    price: string;
+    city: string;
+    km: string;
+    images: string[];
+    uid: string;
+}
 
 export function Dashboard() {
-    return(
+
+    const [cars, setCars] = useState<CarProps[]>([])
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+
+        function loadCars() {
+            if (!user?.uid) {
+                return;
+            }
+
+            const carsRef = collection(db, "cars")
+            const queryRef = query(carsRef, where("uid", "==", user.uid))
+
+            getDocs(queryRef)
+                .then((snapshot) => {
+                    let listCars = [] as CarProps[];
+
+                    snapshot.forEach(doc => {
+                        listCars.push({
+                            id: doc.id,
+                            name: doc.data().name,
+                            year: doc.data().year,
+                            km: doc.data().km,
+                            city: doc.data().city,
+                            price: doc.data().price,
+                            images: doc.data().images,
+                            uid: doc.data().uid
+                        })
+                    })
+
+                    setCars(listCars)
+                })
+        }
+
+        loadCars()
+    }, [user])
+
+    async function handleDeleteCar(id: string) {
+        const docRef = doc(db, "cars", id)
+        await deleteDoc(docRef);
+        setCars(cars.filter(car => car.id !== id))
+    }
+
+    return (
         <Container>
-            <DashboardHeader/>
+            <DashboardHeader />
+
+            <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+
+                {cars.map(car => (
+                    <section className="w-full bg-white rounded-lg relative">
+
+                        <button onClick={() => { handleDeleteCar(car.id) }} className=" cursor-pointer absolute bg-white w-14 h-14 rounded-full flex justify-center items-center right-2 top-2 drop-shadow">
+                            <FiTrash2 size={26} color="#000" />
+                        </button>
+
+                        <img
+                            className="w-full rounded-lg mb-2 max-h-70"
+                            src={car.images[0]}
+                            alt=""
+                        />
+                        <p className="font-bold mt-1 px-2 mb-2">{car.name}</p>
+
+                        <div className="flex flex-col px-2">
+                            <span className="text-zinc-700">
+                                Ano {car.year} | {car.km} km
+                            </span>
+                            <strong className="text-black font-bold mt-4">
+                                R$ {car.price}
+                            </strong>
+                        </div>
+
+                        <div className="w-full h-px bg-slate-200 my-2"></div>
+                        <div className="px-2 pb-2">
+
+
+                            <span className="text-black ">
+                                {car.city} 
+                            </span>
+                        </div>
+                    </section>
+                ))}
+
+            </main>
+
+
         </Container>
-      
+
     )
 }
