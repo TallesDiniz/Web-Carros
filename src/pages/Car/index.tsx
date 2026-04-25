2ea04e8f02216ee3e5e16a1a133dd9feed7d2225
Talles Diniz <tallessdiniz@gmail.com>
2026-04-25 18:24:45 -0300

    feat: criação interface CarProps

--- src/pages/Car/index.tsx
+++ src/pages/Car/index.tsx
@@ -1,4 +1,27 @@
+import { useState, useEffect } from 'react';
+import { Container } from '../../components/Container';
+import { FaWhatsapp } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
+
+interface CarProps {
+    id: string;
+    name: string;
+    model: string;
+    price: string;
+    year: string;
+    km: string;
+    city: string;
+    description: string;
+    created: string;
+    owner: string;
+    uid: string;
+    images: string[];
+    whatsapp: string;
+}
+
 export function CarDetail() {
+    const [car, setCar] = useState<CarProps>()
+
     return(
         <div>
             <h1>Pagina detalhes</h1>
